284717081652066eec7955b0f152719d9c5fe2ed
McLutzifer <lukasschumi@gmx.at>
2021-02-25 21:46:11 +0100

    calculation class

--- ue2.java
+++ ue2.java
@@ -1,9 +1,15 @@
 public class ue2 {
 
+    public static void calculation(int x, int y, String op) {
+        
+    }
 
     public static void main (String args[]) {
 
-        System.out.println("TTesett");
-            
+        double first = Double.parseDouble(args[0]);
+        double second = Double.parseDouble(args[2]); 
+        
+        String opeation = args[1];
+
         }
 }
