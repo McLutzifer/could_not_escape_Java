97d41a673815cc5e2fa9fd4f7e7a406453718ec8
McLutzifer <lukasschumi@gmx.at>
2021-02-25 21:48:42 +0100

    update calculations to double

--- ue2.java
+++ ue2.java
@@ -1,15 +1,18 @@
 public class ue2 {
 
-    public static void calculation(int x, int y, String op) {
-        
+    public static double calculation(double x, double y, String op) {
+        if (op == "+") {
+            
+            return x + y;
+        }
     }
 
     public static void main (String args[]) {
 
         double first = Double.parseDouble(args[0]);
         double second = Double.parseDouble(args[2]); 
-        
-        String opeation = args[1];
+        String operation = args[1];
 
+        calculation(first, second, operation)
         }
 }
