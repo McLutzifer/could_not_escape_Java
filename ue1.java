2509f464d521ce0ccb17c4d537772d9eb810de21
McLutzifer <lukasschumi@gmx.at>
2021-02-25 17:41:16 +0100

    update

--- ue1.java
+++ ue1.java
@@ -1,3 +1,10 @@
 public class ue1 {
-    
+    public static void main (int args[]) {
+        int first = args[0];
+        int second = args[1];
+
+        for (int x = first; x < second, x++) {
+            System.out.println(x);
+        }
+    }
 }
