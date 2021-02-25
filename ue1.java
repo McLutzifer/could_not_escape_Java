e91c1c7c5d50649943c3127d6b0374e70470cbca
McLutzifer <lukasschumi@gmx.at>
2021-02-25 17:47:02 +0100

    String args[]

--- ue1.java
+++ ue1.java
@@ -1,8 +1,8 @@
 public class ue1 {
     
-    public static void main (int args[]) {
-        int first = args[0];
-        int second = args[1];
+    public static void main (String args[]) {
+        int first = Integer.parseInt(args[0]);
+        int second = Integer.parseInt(args[1]);
 
         for (int x = first; x < second; x++) {
             System.out.println(x);
