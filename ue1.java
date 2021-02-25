c81766f47ba7926883db0718ad93047f537299e7
McLutzifer <lukasschumi@gmx.at>
2021-02-25 18:00:14 +0100

    add prime function

--- ue1.java
+++ ue1.java
@@ -1,16 +1,21 @@
 public class ue1 {
-    
+
+
+    static void isPrime(int num) {
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return;
+            }
+            System.out.println(num);
+        }
+    }
+
     public static void main (String args[]) {
         int first = Integer.parseInt(args[0]);
         int second = Integer.parseInt(args[1]);
 
         for (int x = first; x < second; x++) {
-            for (int y = x; y == 2; y--) {
-                if (x % y == 0) {
-                    break;
-                }
-                System.out.println(x);
+            isPrime(x);
             }
         }
-    }
 }
