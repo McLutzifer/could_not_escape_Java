a207f106c73f64232ee9a9548b8441757dd913c7
McLutzifer <lukasschumi@gmx.at>
2021-02-26 10:36:15 +0100

    translate to english

--- ue3.java
+++ ue3.java
@@ -5,15 +5,16 @@ public class ue3 {
 
     public static void main (String args[]) {
 
+        System.out.println("-------------\n--- Insert two numbers to get the sum ---\n--------------");
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Please insert first integer number:");
+        System.out.print("Please insert first integer number >>  ");
         int z1 = sc.nextInt();
-        System.out.print("Please insert first integer number:");
+        System.out.print("Please insert second integer number >>  ");
         int z2 = sc.nextInt();
 
-        System.out.println("Summe ist:");
+        System.out.print("Sum is:  ");
         System.out.println(z1 + z2);
         }
 }
