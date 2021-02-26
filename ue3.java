1e50a44d37672dd7d51e50db30560b8a52510fcf
McLutzifer <lukasschumi@gmx.at>
2021-02-26 10:16:52 +0100

    add ue3

---
+++ ue3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ue3 {
+
+
+    public static void main (String args[]) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bitte nummer eingeben:");
+        int z1 = sc.nextInt();
+        System.out.println("Bitte nummer eingeben:");
+        int z2 = sc.nextInt();
+
+        System.out.println("Summe ist:");
+        System.out.println(z1 + z2);
+        }
+}
