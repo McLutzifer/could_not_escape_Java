34606e1f26d8d0883213e968dab8e82e2a4e4e6f
McLutzifer <lukasschumi@gmx.at>
2021-02-28 17:56:52 +0100

    Add comments

--- First_Exercise_Task_2.java
+++ First_Exercise_Task_2.java
@@ -17,7 +17,7 @@ public class First_Exercise_Task_2 {
                 break;
             default:
                 System.out.println("Please insert valid operation (+, -, x, /)");
-                z = 404;
+                z = 404;        //since function has to return an int value, as a little gag the classic 404 error will be displayed
         }
         return z;
     }
@@ -25,12 +25,12 @@ public class First_Exercise_Task_2 {
     public static void main (String args[]) {
 
         int len = args.length;
-        if (len != 3) {
+        if (len != 3) {    // check if 3 commandline arguments are provided, if not the main function just finishes 
             System.out.println("Please insert two integers and an operation");
             return;
         }
 
-        double first = Double.parseDouble(args[0]);
+        double first = Double.parseDouble(args[0]);  //parse first and third parameter from a String to a double
         double second = Double.parseDouble(args[2]); 
         String operation = args[1];
 
