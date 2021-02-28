7472f60a28d56c0be3c3d5d3214c04055f799bb6
McLutzifer <lukasschumi@gmx.at>
2021-02-28 18:01:41 +0100

    Add comments

--- First_Exercise_Task_3.java
+++ First_Exercise_Task_3.java
@@ -1,4 +1,9 @@
-import java.util.Scanner;
+/*
+Lukas Schumi  //  se201028
+Exercise 1  //  Task 3
+*/
+
+import java.util.Scanner;       // import java.util.Scanner to use Scanner class
 
 public class First_Exercise_Task_3 {
 
@@ -10,13 +15,13 @@ public class First_Exercise_Task_3 {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Please insert first integer number >>  ");
-        int z1 = sc.nextInt();
+        int z1 = sc.nextInt();      //specifying that an int should be read by the scanner
         System.out.print("Please insert second integer number >>  ");
         int z2 = sc.nextInt();
 
         System.out.print("Sum: " + z1 + " + " + z2 + " = " );
         System.out.println(z1 + z2);
 
-        sc.close();
+        sc.close();   // close Scanner class so no resources are leaking
         }
 }
