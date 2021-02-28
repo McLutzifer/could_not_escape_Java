80f1ab3d0e4074cd56eb851555824f296af6b6d0
McLutzifer <lukasschumi@gmx.at>
2021-02-28 12:20:21 +0100

    update

--- First_Exercise_Task_3.java
+++ First_Exercise_Task_3.java
@@ -5,17 +5,6 @@ public class First_Exercise_Task_3 {
 
     public static void main (String args[]) {
 
-<<<<<<< HEAD
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Bitte nummer eingeben:");
-        int z1 = sc.nextInt();
-        System.out.println("Bitte nummer eingeben:");
-        int z2 = sc.nextInt();
-
-        System.out.println("Summe ist:");
-=======
         System.out.println("-------------\n--- Insert two numbers to get the sum ---\n--------------");
 
         Scanner sc = new Scanner(System.in);
@@ -26,7 +15,6 @@ public class First_Exercise_Task_3 {
         int z2 = sc.nextInt();
 
         System.out.print("Sum: " + z1 + " + " + z2 + " = " );
->>>>>>> 822c3e856e5785f0cb9b199d7714925fdd485fc0
         System.out.println(z1 + z2);
         }
 }
