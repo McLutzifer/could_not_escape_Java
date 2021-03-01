ac3fb5ce06fd51a9d001fa3447b8b28e684f7443
McLutzifer <lukasschumi@gmx.at>
2021-03-01 21:13:02 +0100

    import Scanner

--- LearninJava/src/Main.java
+++ LearninJava/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,15 +7,18 @@ public class Main {
         double studentGPA = 3.45;
         String studentFirstName = "Kayla";
         String studentLastName = "Hammon";
-        boolean hasPerfectAttendance = true;
+        //boolean hasPerfectAttendance = true;
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstName.charAt(0));
         System.out.println(studentLastName.charAt(0));
-        System.out.println(hasPerfectAttendance);
 
         System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+
+        // **** input
+        System.out.println("Whatdo you want to update it to?");
+        Scanner input = new Scanner(System.in);
 
     }
 }
