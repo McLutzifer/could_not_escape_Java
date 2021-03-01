eca56d13b843b1704dbe7b1478b59f4e87fb9129
McLutzifer <lukasschumi@gmx.at>
2021-03-01 20:53:27 +0100

    update

--- LearninJava/src/LearnJava.java
+++ LearninJava/src/LearnJava.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class LearnJava {
 
     public static void main(String[] args) {
@@ -6,9 +8,7 @@ public class LearnJava {
         char b = 'b';
         long c = 400;
         double d = 3.2;
-
         String name = "Susan";
-
         System.out.println(name.toUpperCase());
          */
 
@@ -19,6 +19,10 @@ public class LearnJava {
         Animal a = new Animal();
         String dog = a.iAmDog();
         System.out.println(dog);
+
+        ArrayList<Integer> num = new ArrayList<Integer>();
+        num.add(3);
+
 
     }  // end main
 
