22296960bb52776fa2d1f7323aba59380dae2b85
McLutzifer <lukasschumi@gmx.at>
2021-03-01 20:45:24 +0100

    add function

--- LearninJava/src/LearnJava.java
+++ LearninJava/src/LearnJava.java
@@ -1,6 +1,7 @@
 public class LearnJava {
 
     public static void main(String[] args) {
+        /*
         int a = 5;
         char b = 'b';
         long c = 400;
@@ -9,5 +10,13 @@ public class LearnJava {
         String name = "Susan";
 
         System.out.println(name.toUpperCase());
+         */
+
+
+    }  // end main
+
+    public static void addExclamationPoint(String s) {
+        System.out.println(s + "!");
     }
-}
+
+} // end class LearnJava
