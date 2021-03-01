a52dd8af2191453df1a9dce06af1587e7b1d0f59
McLutzifer <lukasschumi@gmx.at>
2021-03-01 20:55:19 +0100

    update Animal

--- LearninJava/src/Animal.java
+++ LearninJava/src/Animal.java
@@ -3,4 +3,15 @@ public class Animal {
     public static String iAmDog() {
         return "I am a dog";
     }
-}
+
+    public static void doStuff() {
+
+        int a = 5;
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(i);
+        }
+    }
+
+}  // end class Animal
+
