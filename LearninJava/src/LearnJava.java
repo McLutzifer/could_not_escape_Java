05903ae3795b9b96a84087edd149f637ce71611a
McLutzifer <lukasschumi@gmx.at>
2021-03-08 20:53:26 +0100

    point object

--- LearninJava/src/LearnJava.java
+++ LearninJava/src/LearnJava.java
@@ -35,6 +35,8 @@ public class LearnJava {
         //reference
         Point point1 = new Point(1, 1);
         Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
 
 
 
