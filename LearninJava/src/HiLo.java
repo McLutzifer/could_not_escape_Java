27cadde13c66c92db7aa851a217268b2b8ef373a
McLutzifer <lukasschumi@gmx.at>
2021-03-11 21:15:38 +0100

    update HiLo add guess

--- LearninJava/src/HiLo.java
+++ LearninJava/src/HiLo.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class HiLo {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         // Create a random number for the user to guess
         int theNumber = (int)(Math.random() * 100 + 1);
+        System.out.println( theNumber );
+        int guess = 0;
+        System.out.println("Guess a umber between 1 and 100");
 
-        Scanner scan = new Scanner(System.in);
 
     }
 }
