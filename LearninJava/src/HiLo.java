eb3c877d8d7c24ac0b906cf93c5e3693b382987a
McLutzifer <lukasschumi@gmx.at>
2021-03-11 21:14:13 +0100

    update HiLo add Scanner

--- LearninJava/src/HiLo.java
+++ LearninJava/src/HiLo.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class HiLo {
     public static void main(String[] args) {
         // Create a random number for the user to guess
         int theNumber = (int)(Math.random() * 100 + 1);
+
+        Scanner scan = new Scanner(System.in);
 
     }
 }
