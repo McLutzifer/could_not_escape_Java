8277dfa9c7a844eabc7a29748ee320dd1b759584
McLutzifer <lukasschumi@gmx.at>
2021-03-12 19:35:57 +0100

    Add do while loop

--- LearninJava/src/HiLo.java
+++ LearninJava/src/HiLo.java
@@ -3,20 +3,25 @@ import java.util.Scanner;
 public class HiLo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        // Create a random number for the user to guess
-        int theNumber = (int)(Math.random() * 100 + 1);
-        System.out.println( theNumber );
-        int guess = 0;
-        while (guess != theNumber) {
-            System.out.println("Guess a umber between 1 and 100");
-            guess = scan.nextInt();
-            //System.out.println("you entered " + guess + ".");
-            if (guess < theNumber)
-                System.out.println(guess + " is too low. Try again.");
-            else if (guess > theNumber)
-                System.out.println(guess + " is too high. Try again.");
-            else
-                System.out.println(guess + " id correct. You win!");
-        } // end of while loop for guessing
+        String playAgain = "";
+        do {
+            // Create a random number for the user to guess
+            int theNumber = (int) (Math.random() * 100 + 1);
+            System.out.println(theNumber);
+            int guess = 0;
+            while (guess != theNumber) {
+                System.out.println("Guess a umber between 1 and 100");
+                guess = scan.nextInt();
+                //System.out.println("you entered " + guess + ".");
+                if (guess < theNumber)
+                    System.out.println(guess + " is too low. Try again.");
+                else if (guess > theNumber)
+                    System.out.println(guess + " is too high. Try again.");
+                else
+                    System.out.println(guess + " id correct. You win!");
+            } // end of while loop for guessing
+            System.out.println("Would you like to play again (y/n)?");
+            playAgain = scan.next();
+        } while (playAgain.equalsIgnoreCase("y"));
     } // end main
 } // end class HiLo
--- LearninJava/src/Prog613.java
+++ LearninJava/src/Prog613.java
@@ -1,12 +1,15 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Prog613 {
 
     public static void main(String[] args) {
 
+        Frame613 f = new Frame613();
+        f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        f.setVisible( true );
     }
 }
-
 
 class Frame613 extends JFrame {
     public Frame613() {
@@ -14,10 +17,15 @@ class Frame613 extends JFrame {
         setSize(300,200);
         setLocation(100,200);
 
-
+        Panel613 panel = new Panel613();
+        Container cp = getContentPane();
+        cp.add(panel);
     }
 }
 
 class Panel613 extends JPanel {
-    public void
+    public void paintComponent( Graphics g) {
+        super.paintComponent(g);
+        g.drawString("Hi", 75,100);
+    }
 }
