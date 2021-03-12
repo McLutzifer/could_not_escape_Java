ac75a9a83af4c01ea784f6104d3e49d4eb034ffb
McLutzifer <lukasschumi@gmx.at>
2021-03-12 19:19:33 +0100

    update Prog611

--- LearninJava/src/Prog611.java
+++ LearninJava/src/Prog611.java
@@ -5,5 +5,13 @@ public class Prog611 {
     public static void main(String[] args) {
 
         String name = JOptionPane.showInputDialog("What's your name?");
+
+        String input = JOptionPane.showInputDialog("How old are you?");
+        int age = Integer.parseInt(input);
+
+        System.out.print( "Hello, " + name + ".");
+        System.out.println("Next year, you'll be " + (age+1) );
+
+        System.exit(0);
     } // end main
 } // end class Prog611
