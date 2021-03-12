cb12e1e42b581aca7c513d5d4b1d76c28fc84be0
McLutzifer <lukasschumi@gmx.at>
2021-03-12 19:17:26 +0100

    create Prog611

---
+++ LearninJava/src/Prog611.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Prog611 {
+
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("What's your name?");
+    } // end main
+} // end class Prog611
