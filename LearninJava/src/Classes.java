1447614e87142e7240b345e903d04eb60fc2fabb
McLutzifer <lukasschumi@gmx.at>
2021-03-12 15:44:54 +0100

    Add classes

--- LearninJava/src/Classes.java
+++ LearninJava/src/Classes.java
@@ -1,8 +1,20 @@
+class A {
+    public A() {
+        System.out.println("New A");
+    }
+}
+
+class B extends A {
+    public B() {
+        System.out.println("New B");
+    }
+}
+
+
 public class Classes {
-
     public static void main(String[] args) {
-        // hoe create instances? Constructor!
-
+        // how create instances? Constructor!
+        B obj = new B();
     } // end main
 } // end cass Classes
 
