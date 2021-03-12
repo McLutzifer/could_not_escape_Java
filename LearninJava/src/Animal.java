5fabf00638cb37f0b6ad34f83ad5379b52b36349
McLutzifer <lukasschumi@gmx.at>
2021-03-12 15:54:31 +0100

    Add polimorphism

--- LearninJava/src/Animal.java
+++ LearninJava/src/Animal.java
@@ -1,21 +1,21 @@
 public class Animal {
 
     protected int legs;
+
     public void eat() {
         System.out.println("Animals eat");
     }
-
     public static String iAmDog() {
         return "I am a dog";
     }
-
     public static void doStuff() {
-
         int a = 5;
-
         for (int i = 0; i < 7; i++) {
             System.out.println(i);
         }
+    }
+    public void makeSound() {
+        System.out.println("Grr...");
     }
 
 }  // end class Animal
@@ -23,5 +23,14 @@ public class Animal {
 class Dog extends Animal {
     Dog() {
         legs = 4;
+    }
+    public void makeSound() {
+        System.out.println("Woof");
+    }
+}
+
+class Cat extends Animal {
+    public void makeSound() {
+        System.out.println("Meow");
     }
 }
