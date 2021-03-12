6ee50c3316cd9a5dc31bfe886a4520cb64f90e72
McLutzifer <lukasschumi@gmx.at>
2021-03-12 19:23:55 +0100

    update Prog613

---
+++ LearninJava/src/Prog613.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Prog613 {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Frame613 extends JFrame {
+    public Frame613() {
+        setTitle("613 rocks!");
+        setSize(300,200);
+        setLocation(100,200);
+
+
+    }
+}
+
+class Panel613 extends JPanel {
+    public void
+}
