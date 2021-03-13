1d023be2bba2821070b3146ac0dd2ede0390d0b8
McLutzifer <lukasschumi@gmx.at>
2021-03-13 10:05:10 +0100

    update

--- Habelitz/src/rheinwerk/Selektion1.java
+++ Habelitz/src/rheinwerk/Selektion1.java
@@ -6,7 +6,12 @@ public class Selektion1 {
     public static void main(String[] args) {
 
         var eingabe = JOptionPane.showInputDialog("Geben Sie Ihr Alter ein: ");
-        var
-    }
+        var alter = Integer.parseInt(eingabe);
 
+        if (alter < 18) {
+            JOptionPane.showMessageDialog(null, "Sorry, noch nicht volljhrig!");
+        } else {
+            JOptionPane.showMessageDialog(null,"Glückwunsch! Sie sind volljährig!");
+        }
+    }
 }
