2c357dd78125561e23e3f731bba1e8cc440af2c1
McLutzifer <lukasschumi@gmx.at>
2021-03-13 09:26:28 +0100

    update

--- Habelitz/src/rheinwerk/Selektion1.java
+++ Habelitz/src/rheinwerk/Selektion1.java
@@ -3,7 +3,10 @@ package rheinwerk;
 import javax.swing.*;
 
 public class Selektion1 {
+    public static void main(String[] args) {
 
-    var eingabe = JOptionPane.showInputDialog("Geben Sie Ihr Alter ein: ");
+        var eingabe = JOptionPane.showInputDialog("Geben Sie Ihr Alter ein: ");
+        var
+    }
 
 }
