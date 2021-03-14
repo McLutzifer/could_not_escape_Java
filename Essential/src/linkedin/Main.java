fa05142c3e122e0e43312fdf76cc505266f5aef2
McLutzifer <lukasschumi@gmx.at>
2021-03-14 20:30:25 +0100

    just testing stuff

--- Essential/src/linkedin/Main.java
+++ Essential/src/linkedin/Main.java
@@ -1,8 +1,14 @@
 package linkedin;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please insert double");
+        double d = sc.nextDouble();
+        System.out.println("double of double is " + d*2);
     }
 }
