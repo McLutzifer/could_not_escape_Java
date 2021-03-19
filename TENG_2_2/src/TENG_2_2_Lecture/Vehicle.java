55f760ad77bcdb08cbee57b34e2b82b575cdba7a
McLutzifer <lukasschumi@gmx.at>
2021-03-19 14:36:10 +0100

    add constructor

--- TENG_2_2/src/TENG_2_2_Lecture/Vehicle.java
+++ TENG_2_2/src/TENG_2_2_Lecture/Vehicle.java
@@ -6,4 +6,11 @@ public class Vehicle {
     private float speed;
     private Color color;
     private String brand;
+
+    // Construct an empty object
+    public Vehicle() {
+        this.speed = 0.0f;
+        this.color = new Color(255,255,255);
+        this.brand =
+    }
 }
