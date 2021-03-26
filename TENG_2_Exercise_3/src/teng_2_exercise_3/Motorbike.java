4075ce7dfd1bb9e2c7a4b929e4854f5185dcf3f0
McLutzifer <lukasschumi@gmx.at>
2021-03-26 16:10:59 +0100

    update

---
+++ TENG_2_Exercise_3/src/teng_2_exercise_3/Motorbike.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teng_2_exercise_3;
+
+/**
+ *
+ * @author lbwagnerm
+ */
+public class Motorbike extends Vehicle {
+    
+    // Insert your code here!
+    
+}
