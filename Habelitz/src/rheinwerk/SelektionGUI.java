68ff27273eaa038b22448fff70bfa7c14ed3880f
McLutzifer <lukasschumi@gmx.at>
2021-04-01 14:33:33 +0200

    javax.swing.*

--- Habelitz/src/rheinwerk/SelektionGUI.java
+++ Habelitz/src/rheinwerk/SelektionGUI.java
@@ -1,4 +1,14 @@
 package rheinwerk;
 
+import javax.swing.*;
+
 public class SelektionGUI {
+    public static void main(String[] args) {
+        var eingabe = JOptionPane.showInputDialog("Geben Sie Ihr Alter ein:");
+        var alter = Integer.parseInt(eingabe);
+        if (alter < 18) {
+
+        }
+    }
+
 }
