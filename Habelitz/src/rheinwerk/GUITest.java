95197cc367d46eca401964ca151a8e254e8bf597
McLutzifer <lukasschumi@gmx.at>
2021-04-01 19:54:23 +0200

    test

--- Habelitz/src/rheinwerk/GUITest.java
+++ Habelitz/src/rheinwerk/GUITest.java
@@ -1,7 +1,10 @@
 package rheinwerk;
 
+import javax.swing.*;
+
 public class GUITest {
     public static void main(String[] args) {
-
+        var eingabe = JOptionPane.showInputDialog("Steht ier der Text?");
+        var alter = Integer.parseInt(eingabe);
     }
 }
