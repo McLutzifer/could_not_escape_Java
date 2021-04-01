2d247a187e20b2afb779968fc59de29c4f09a98a
McLutzifer <lukasschumi@gmx.at>
2021-04-01 19:55:44 +0200

    test

--- Habelitz/src/rheinwerk/GUITest.java
+++ Habelitz/src/rheinwerk/GUITest.java
@@ -6,6 +6,6 @@ public class GUITest {
     public static void main(String[] args) {
         var eingabe = JOptionPane.showInputDialog("Bitte Zahl eingeben");
         var alter = Integer.parseInt(eingabe);
-        JOptionPane.showMessageDialog("Das ");
+        JOptionPane.showMessageDialog(null, "Das ");
     }
 }
