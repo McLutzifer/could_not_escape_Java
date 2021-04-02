d485add42b4cc9c609241fea1d0ec56e2448faaf
McLutzifer <lukasschumi@gmx.at>
2021-04-02 21:43:20 +0200

    update

--- LearninJava/src/GuessingGame.java
+++ LearninJava/src/GuessingGame.java
@@ -3,4 +3,9 @@ import javax.swing.*;
 public class GuessingGame extends JFrame {
     private JTextField txtGuess;
     private JLabel lblOutput;
+    private int theNumber;
+
+    public void checkGuess(){
+        String guessText = txtGuess.getText();
+    } // end checkGuess
 }
