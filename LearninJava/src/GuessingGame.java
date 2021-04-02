d5cbb15ff7f8750c6cfa12c9674aab66fd79ac0c
McLutzifer <lukasschumi@gmx.at>
2021-04-02 21:51:14 +0200

    update

--- LearninJava/src/GuessingGame.java
+++ LearninJava/src/GuessingGame.java
@@ -13,7 +13,22 @@ public class GuessingGame extends JFrame {
             if (guess < theNumber)
                 message = guess + " is too low. Try again.";
             else if (guess > theNumber)
-                message = guess + " is too high. Try again"
+                message = guess + " is too high. Try again";
+            else {
+                message = guess +
+                        " is correct. You win! Let's play again.";
+                newGame();
+            }
+        } catch (Exception e) {
+            message = "Enter a whole number between 1 and 100";
+        } finally {
+            lblOutput.setText(message);
+            txtGuess.requestFocus();
+            txtGuess.selectAll();
         }
     } // end checkGuess
+
+    public void newGame() {
+
+    } // end newGame
 }
