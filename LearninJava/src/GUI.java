8542fc8137033e52399d1f1206b9f9daaef4b94b
McLutzifer <lukasschumi@gmx.at>
2021-04-02 21:28:38 +0200

    fix typo

--- LearninJava/src/GUI.java
+++ LearninJava/src/GUI.java
@@ -4,6 +4,6 @@ public class GUI {
     public static void main(String[] args) {
         JFrame window = new JFrame("Bryson's Window");
         JPanel panel = new JPanel();
-        JButton button = new Button("Click me!");
+        JButton button = new JButton("Click me!");
     }
 }
