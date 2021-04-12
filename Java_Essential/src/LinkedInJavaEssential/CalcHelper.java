8ffded40f033a94c79cf0e87da779a945ef9b2fd
McLutzifer <lukasschumi@gmx.at>
2021-04-12 18:14:04 +0200

    add addValues

--- Java_Essential/src/LinkedInJavaEssential/CalcHelper.java
+++ Java_Essential/src/LinkedInJavaEssential/CalcHelper.java
@@ -5,4 +5,16 @@ public class CalcHelper {
     public static double addValues(double d1, double d2) {
         return d1 + d2;
     }
+
+    public static double subtractValues(double d1, double d2) {
+        return d1 - d2;
+    }
+
+    public static double multiplyValues(double d1, double d2) {
+        return d1 * d2;
+    }
+
+    public static double devideValues(double d1, double d2) {
+        return d1 / d2;
+    }
 }
