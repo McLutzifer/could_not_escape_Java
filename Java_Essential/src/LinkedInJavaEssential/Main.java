cb7099d306ef07fceccc564a2ba4bf2a3d2bc192
McLutzifer <lukasschumi@gmx.at>
2021-04-12 18:17:38 +0200

    update

--- Java_Essential/src/LinkedInJavaEssential/Main.java
+++ Java_Essential/src/LinkedInJavaEssential/Main.java
@@ -1,11 +1,15 @@
 package LinkedInJavaEssential;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        var sc = new Scanner(System.in);
         System.out.println("Test");
 
         double x  = CalcHelper.addValues(2, 4);
 
-    }
-}
+
+    } // end main
+} // end class Main
