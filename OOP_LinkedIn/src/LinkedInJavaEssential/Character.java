5e30d0381d3a3e4c6a3383e2637fa7e8303f6ae2
McLutzifer <lukasschumi@gmx.at>
2021-04-13 13:06:06 +0200

    update

--- OOP_LinkedIn/src/LinkedInJavaEssential/Character.java
+++ OOP_LinkedIn/src/LinkedInJavaEssential/Character.java
@@ -1,4 +1,20 @@
 package LinkedInJavaEssential;
 
+import com.sun.jdi.event.StepEvent;
+
 public class Character {
+
+    private String name;
+    private String species;
+
+    public Character(String nom, String spec) {
+        this.name = nom;
+        this.species = spec;
+    }
+
+    public static void main(String[] args) {
+        Character bilbo = new Character("Bilbo", "Hobbit");
+
+        System.out.println(bilbo.name);
+    }
 }
