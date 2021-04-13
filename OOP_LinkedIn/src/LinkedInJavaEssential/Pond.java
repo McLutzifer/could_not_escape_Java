4a46276eaabc9ad2f8e4b92a6e9a151c13eeee90
McLutzifer <lukasschumi@gmx.at>
2021-04-13 13:16:40 +0200

    update

--- OOP_LinkedIn/src/LinkedInJavaEssential/Pond.java
+++ OOP_LinkedIn/src/LinkedInJavaEssential/Pond.java
@@ -2,6 +2,8 @@ package LinkedInJavaEssential;
 
 public class Pond {
     public static void main(String[] args) {
-        System.out.println("TEST");
+        System.out.println("TEST for POND");
+
+        Duck mobyDuck = new Duck("Moby", 90/3, "celery");
     }
 }
