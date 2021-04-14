8202da688b09b74df532364b9718083058b49990
McLutzifer <lukasschumi@gmx.at>
2021-04-14 18:45:06 +0200

    create Constructors.java

--- exam_prep/src/TENG_3_Derived_Classs/Constructors.java
+++ exam_prep/src/TENG_3_Derived_Classs/Constructors.java
@@ -1,4 +1,17 @@
 package TENG_3_Derived_Classs;
 
 public class Constructors {
+    int x; // class attribute
+    int y;
+    int z;
+
+    public Constructors() {
+        x = 5;
+    }
+
+    public Constructors(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 }
