1e7ed30a6fa6fac05c90c59d0dbe108f455e32b8
McLutzifer <lukasschumi@gmx.at>
2021-04-14 18:17:53 +0200

    update

--- exam_prep/src/TENG_3_Derived_Classs/Arrays.java
+++ exam_prep/src/TENG_3_Derived_Classs/Arrays.java
@@ -1,11 +1,7 @@
 package TENG_3_Derived_Classs;
 
 public class Arrays {
-    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    static String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
     int[] myNum = {10, 20, 30, 40};
 }
 
-
-    public static void main(String[] args) {
-        System.out.println(cars[0]);
-    }
