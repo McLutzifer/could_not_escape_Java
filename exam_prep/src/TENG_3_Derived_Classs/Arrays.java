3854321522b1cd891936e4ed7e8eed9525f1fc93
McLutzifer <lukasschumi@gmx.at>
2021-04-14 18:16:33 +0200

    Add Arrays

--- exam_prep/src/TENG_3_Derived_Classs/Arrays.java
+++ exam_prep/src/TENG_3_Derived_Classs/Arrays.java
@@ -4,3 +4,8 @@ public class Arrays {
     String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
     int[] myNum = {10, 20, 30, 40};
 }
+
+
+    public static void main(String[] args) {
+        System.out.println(cars[0]);
+    }
