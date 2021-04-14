df7dc6f6698cab9c5f884a9abfdbd60e2c24aa7e
McLutzifer <lukasschumi@gmx.at>
2021-04-14 18:39:17 +0200

    create public array

--- exam_prep/src/TENG_3_Derived_Classs/NotStatic.java
+++ exam_prep/src/TENG_3_Derived_Classs/NotStatic.java
@@ -2,6 +2,6 @@ package TENG_3_Derived_Classs;
 
 public class NotStatic {
 
-    String[] fruit = {"Banana", "Apple", "Strawberry"};
+    public String[] fruit = {"Banana", "Apple", "Strawberry"};
 
 }
