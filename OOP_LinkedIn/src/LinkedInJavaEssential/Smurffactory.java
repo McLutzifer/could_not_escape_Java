df449ddce761061271d0ff839a06ce348fd9dc06
McLutzifer <lukasschumi@gmx.at>
2021-04-16 10:34:33 +0200

    update

--- OOP_LinkedIn/src/LinkedInJavaEssential/Smurffactory.java
+++ OOP_LinkedIn/src/LinkedInJavaEssential/Smurffactory.java
@@ -2,6 +2,10 @@ package LinkedInJavaEssential;
 
 public class Smurffactory {
     public static void main(String[] args) {
-
+        Smurf.createSmurf("handy");
+        Smurf.createSmurf("handy");
+        Smurf.createSmurf("handy");
+        Smurf.createSmurf("handy");
+        Smurf.createSmurf("handy");
     }
 }
