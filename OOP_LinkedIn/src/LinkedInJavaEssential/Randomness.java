2993656550849f1ce44f353fea9bddc97a562479
McLutzifer <lukasschumi@gmx.at>
2021-04-16 10:37:44 +0200

    create Randomness.java

--- OOP_LinkedIn/src/LinkedInJavaEssential/Randomness.java
+++ OOP_LinkedIn/src/LinkedInJavaEssential/Randomness.java
@@ -7,5 +7,7 @@ public class Randomness {
     public static void main(String[] args) {
         Random random = new Random();
         System.out.println((random.nextInt()));
+        System.out.println(random.nextBoolean());
+        System.out.println(random.nextDouble());
     }
 }
