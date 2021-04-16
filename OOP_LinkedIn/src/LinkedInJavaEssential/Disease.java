bf5c08e0f04c9749f2af7c12285411d25f741a1d
McLutzifer <lukasschumi@gmx.at>
2021-04-16 09:30:56 +0200

    create disease

---
+++ OOP_LinkedIn/src/LinkedInJavaEssential/Disease.java
@@ -0,0 +1,12 @@
+package LinkedInJavaEssential;
+
+public class Disease {
+
+    private String name;
+    private boolean curable;
+
+    Disease(String title, boolean isCurable) {
+        this.curable = isCurable;
+        this.name = title;
+    }
+}
