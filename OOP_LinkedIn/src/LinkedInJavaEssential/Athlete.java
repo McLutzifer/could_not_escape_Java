21d8778152e84a44041890156ceda3a2b18aa12e
McLutzifer <lukasschumi@gmx.at>
2021-04-16 09:53:21 +0200

    update

--- OOP_LinkedIn/src/LinkedInJavaEssential/Athlete.java
+++ OOP_LinkedIn/src/LinkedInJavaEssential/Athlete.java
@@ -1,4 +1,17 @@
 package LinkedInJavaEssential;
 
 public class Athlete {
+
+    private String name;
+    private int speed;
+
+    public Athlete(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString() {
+        return "Athlete " + name + " is running at " + speed + " miles per hour.";
+    }
 }
--- OOP_LinkedIn/src/LinkedInJavaEssential/Marathon.java
+++ OOP_LinkedIn/src/LinkedInJavaEssential/Marathon.java
@@ -3,6 +3,10 @@ package LinkedInJavaEssential;
 public class Marathon {
 
     public static void main(String[] args) {
-        Athlete mike = new Athlete();
+        Athlete mike = new Athlete("Mike", 10);
+        Athlete betsy = new Athlete("Betsy", 12);
+
+        System.out.println(mike);
+        System.out.println(betsy);
     }
 }
