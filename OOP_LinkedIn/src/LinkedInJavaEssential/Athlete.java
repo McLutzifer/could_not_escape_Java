db19712d7a2e8fb01934fa143ae122484c6eb8f0
McLutzifer <lukasschumi@gmx.at>
2021-04-16 09:59:42 +0200

    update

--- OOP_LinkedIn/src/LinkedInJavaEssential/Athlete.java
+++ OOP_LinkedIn/src/LinkedInJavaEssential/Athlete.java
@@ -4,11 +4,15 @@ public class Athlete {
 
     private String name;
     private int speed;
-    private String marathonLocation;
+    private static String marathonLocation;
+    private int bibNumber;
+    private static int runnersInRace;
 
     public Athlete(String name, int speed) {
         this.name = name;
         this.speed = speed;
+        runnersInRace++;
+        this.bibNumber = runnersInRace;
     }
 
     public void setMarathonLocation(String marathonLocation) {
@@ -17,7 +21,7 @@ public class Athlete {
 
     @Override
     public String toString() {
-        return "Athlete " + name + " is running at " + speed +
+        return "Athlete Nr " + bibNumber + ", " + name + " is running at " + speed +
                 " miles per hour at " + marathonLocation;
     }
 }
