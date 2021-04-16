127ccc9f36b3bb154196c8d650a7eaba3233f8ad
McLutzifer <lukasschumi@gmx.at>
2021-04-16 10:36:31 +0200

    create Randomness

---
+++ OOP_LinkedIn/src/LinkedInJavaEssential/Randomness.java
@@ -0,0 +1,11 @@
+package LinkedInJavaEssential;
+
+import java.util.Random;
+
+public class Randomness {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println((random.nextInt()));
+    }
+}
